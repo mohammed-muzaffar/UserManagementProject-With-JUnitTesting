2d1fc6d737efedbe2285eb001729130b101dce30
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-11-14 00:55:23 +0530

    Swagger UI Integration Complete

--- src/main/java/com/vff/SpringBootModelMappingApplication.java
+++ src/main/java/com/vff/SpringBootModelMappingApplication.java
@@ -1,12 +1,24 @@
 package com.vff;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot Rest API Documentation",
+				description = "Spring Boot Restful Application",
+				version = "v1.0",
+				contact = @Contact(
+						name="Mohammed Muzaffar",
+						email = "mohammedmuzaffar294@gmail.com",
+						url="https://viewfullform.com"
+				)
+		)
+)
 public class SpringBootModelMappingApplication {
 
 	public static void main(String[] args) {
